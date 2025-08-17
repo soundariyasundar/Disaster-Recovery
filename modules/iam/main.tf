132b8b262ae8c41cbd9b3dc50c1721b7ab17b68d
soundariya sundar <154726977+soundariyasundar@users.noreply.github.com>
2025-08-18 02:06:47 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 resource "aws_iam_role" "cluster_role" {
   name = var.iam_role
 
