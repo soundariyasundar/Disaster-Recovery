bdafa46c063f48dfa139597ffcfd14a705dacc30
soundariyasundar <sundarsoundariya@gmail.com>
2025-08-18 12:07:06 +0530

    Resolved merge

--- providers.tf
+++ providers.tf
@@ -1,15 +1,5 @@
 terraform {
-<<<<<<< HEAD
-  
-=======
-  backend "s3" {
-    bucket = "s3backendstatefile12041204"
-    dynamodb_table = "terraform-locks"
-    key = "mystatefile/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-  }
->>>>>>> 7c8e52474c6a1f3c12aa893475b31af542cad3c0
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
