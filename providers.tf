db96f61e186e96053d475fe73b176930c5c79638
soundariyasundar <sundarsoundariya@gmail.com>
2025-08-18 10:50:57 +0530

    deleted

--- providers.tf
+++ providers.tf
@@ -1,11 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket = "s3backendstate1204"
-    dynamodb_table = "terraform-locks"
-    key = "mystatefile/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
